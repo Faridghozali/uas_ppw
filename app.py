cbbe842b2794fbf571cb85c70add610cc24cc015
Datamining <111824123+Faridghozali@users.noreply.github.com>
2023-12-14 07:54:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,24 @@ import numpy as np
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 
 Data, lda, Model, Implementasi = st.tabs(['Data', 'LDA', 'Modelling', 'Implementasi'])
 
 with Data:
-   st.title("UAS Pencarian & Penambangan Web A")
-   st.text("Okhi Sahrul Barkah - 210411100112")
+   st.title("Impelementasi Latent Dirichlet Allocation (LDA) ")
+   st.text("Arbil Shofiyurrahman - 210411100016")
    st.subheader("Deskripsi Data")
-   st.write("Dimana Fitur yang ada di dalam data tersebut diantaranya:")
-   st.text("1) Date\n2) Title\n3) Content\n4) Label")
+   st.write("Fitur Fitur yang ada diantaranya:")
+   st.text("Fitur-fitur data:")
+   st.text("1) Judul")
+   st.text("2) Isi")
+   st.text("3) Label")
    st.subheader("Data")
-   data = pd.read_csv("https://gist.githubusercontent.com/khikisb/db966a30f5341a31d8429885ad522e82/raw/90e5bdecaa24a3bf1a0a4f45b70d51274e7a337f/data_label_news.csv")
+   data = pd.read_csv("berita.csv")
    st.write(data)
 
 with lda:
@@ -40,7 +43,7 @@ with lda:
       submit() 
 
 with Model:
-    tf = pd.read_csv("df_tf.csv")
+    tf = pd.read_csv("berita_tf.csv")
     st.subheader("Jumlah Topik yang Anda Gunakan : " + str(topik))
     st.write("Jika pada menu LDA tidak menentukan jumlah topiknya maka proses modelling akan di default dengan jumlah topik = 5")
     lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1, random_state=42, max_iter=1)
@@ -56,10 +59,10 @@ with Model:
     y = df['Label'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    model1 = KNeighborsClassifier(5)
-    # Pelatihan model KNN dengan data pelatihan
+    # Pelatihan model Random Forest dengan data pelatihan
+    model1 = RandomForestClassifier(n_estimators=100, random_state=42)
     model1.fit(X_train, y_train)
-
+   
     model2 = MultinomialNB()
     # Pelatihan model Naive Bayes dengan data pelatihan
     model2.fit(X_train, y_train)
@@ -69,14 +72,14 @@ with Model:
     model3.fit(X_train, y_train)
 
     st.write("Pilih metode yang ingin anda gunakan :")
-    met1 = st.checkbox("KNN")
+    met1 = st.checkbox("Random Forest")
     met2 = st.checkbox("Naive Bayes")
     met3 = st.checkbox("Decision Tree")
     submit2 = st.button("Pilih")
 
     if submit2:      
         if met1:
-            st.write("Metode yang Anda gunakan Adalah KNN")
+            st.write("Metode yang Anda gunakan Adalah Random Forest")
             # Prediksi label kelas pada data pengujian
             y_pred = model1.predict(X_test)
             # Mengukur akurasi model
@@ -100,7 +103,7 @@ with Model:
             st.write("Anda Belum Memilih Metode")
 
 with Implementasi:
-    data = pd.read_csv("https://gist.githubusercontent.com/khikisb/db966a30f5341a31d8429885ad522e82/raw/90e5bdecaa24a3bf1a0a4f45b70d51274e7a337f/data_label_news.csv")
+    data = pd.read_csv("berita.csv")
     data['Content'].fillna("", inplace=True)
     count_vectorizer = CountVectorizer(max_df=0.95, min_df=2)
     
@@ -128,18 +131,18 @@ with Implementasi:
         return cleaned_text
 
     st.subheader("Implementasi")
-    st.write("Masukkan Berita yang Ingin Dianalisis:")
-    user_abstract = st.text_area("Abstrak", "")
+    st.write("Masukkan Content yang Ingin Dianalisis:")
+    user_content = st.text_area("Content", "")
 
-    if user_abstract:
-        # Preproses abstrak
-        preprocessed_abstract = preprocess_text(user_abstract)
+    if user_content:
+        # Preproses content
+        preprocessed_content = preprocess_text(user_content)
 
         # Fit vocabulary dengan data latih
-        count_vectorizer.fit(data['Abstrak'])
+        count_vectorizer.fit(data['Content'])
 
         # Transform abstrak pengguna dengan count_vectorizer
-        user_tf = count_vectorizer.transform([preprocessed_abstract])
+        user_tf = count_vectorizer.transform([preprocessed_content])
        
         if lda_model is None:
             lda_model = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1, random_state=42, max_iter=1)
@@ -151,4 +154,3 @@ with Implementasi:
         st.write(user_topic_distribution)
         y_pred = model2.predict(user_topic_distribution)
         y_pred
-
