64379949ed35cc2e2a03a74a8bb16f42d5182376
Datamining <111824123+Faridghozali@users.noreply.github.com>
2023-12-14 07:27:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
